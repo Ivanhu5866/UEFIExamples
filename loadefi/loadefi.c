4972d4896ec435e5043cad74d4a1697b41da3046
Ivan Hu <ivan.hu@canonical.com>
2019-08-28 04:18:58 -0400

    loadefi: add install parameter and loadoption
    Adding EFI_SHELL_PARAMETERS_PROTOCOL and loadoption
    which can be used to pass the Argv to the efi application
    which is loaded.
    
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- loadefi/loadefi.c
+++ loadefi/loadefi.c
@@ -20,6 +20,47 @@
 #include <efi.h>
 #include <efilib.h>
 
+#define EFI_SHELL_PARAMETERS_PROTOCOL_GUID \
+  { \
+  0x752f3136, 0x4e16, 0x4fdc, { 0xa2, 0x2a, 0xe5, 0xf4, 0x68, 0x12, 0xf4, 0xca } \
+  }
+
+typedef VOID *SHELL_FILE_HANDLE;
+
+typedef struct _EFI_SHELL_PARAMETERS_PROTOCOL {
+  ///
+  /// Points to an Argc-element array of points to NULL-terminated strings containing
+  /// the command-line parameters. The first entry in the array is always the full file
+  /// path of the executable. Any quotation marks that were used to preserve
+  /// whitespace have been removed.
+  ///
+  CHAR16 **Argv;
+
+  ///
+  /// The number of elements in the Argv array.
+  ///
+  UINTN Argc;
+
+  ///
+  /// The file handle for the standard input for this executable. This may be different
+  /// from the ConInHandle in EFI_SYSTEM_TABLE.
+  ///
+  SHELL_FILE_HANDLE StdIn;
+
+  ///
+  /// The file handle for the standard output for this executable. This may be different
+  /// from the ConOutHandle in EFI_SYSTEM_TABLE.
+  ///
+  SHELL_FILE_HANDLE StdOut;
+
+  ///
+  /// The file handle for the standard error output for this executable. This may be
+  /// different from the StdErrHandle in EFI_SYSTEM_TABLE.
+  ///
+  SHELL_FILE_HANDLE StdErr;
+} EFI_SHELL_PARAMETERS_PROTOCOL;
+
+
 EFI_STATUS
 efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 {
@@ -30,7 +71,46 @@ efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 	EFI_STATUS status;
     	EFI_HANDLE image_handle = NULL;
 
+  	static const EFI_GUID EfiShellParametersProtocolGuid
+		= EFI_SHELL_PARAMETERS_PROTOCOL_GUID;
+
+	EFI_SHELL_PARAMETERS_PROTOCOL *EfiShellParametersProtocol = NULL;
+	INTN i;
+
 	InitializeLib(image, systab);
+
+	status = uefi_call_wrapper(systab->BootServices->OpenProtocol, 6,
+		             image,
+		             (EFI_GUID*)&EfiShellParametersProtocolGuid,
+		             (VOID **)&EfiShellParametersProtocol,
+		             image,
+		             NULL,
+		             EFI_OPEN_PROTOCOL_GET_PROTOCOL
+		             );
+
+	if (!EFI_ERROR(status))
+	{
+		// shell 2.0 interface
+		// hard code argv0, argv1 and pass to the efi applicaiton
+		// which be loaded.
+		EfiShellParametersProtocol->Argc = 2;
+		EfiShellParametersProtocol->Argv[0] = L"test.efi";
+		EfiShellParametersProtocol->Argv[1] = L"-status";
+		Print(L"loadefi:\n");
+		Print(L"  Argc=%d\n", EfiShellParametersProtocol->Argc);
+		for (i = 0 ; i < EfiShellParametersProtocol->Argc ; ++i)
+			Print(L"  Argv[%d] = '%s'\n", i, EfiShellParametersProtocol->Argv[i]);
+	} else {
+		// shell 2.0 FI_SHELL_PARAMETERS_PROTOCOL_GUID open fail
+		// this occurs when this application does not run under
+		// ueif shell but run with bootentry instead
+		Print(L"No EFI_SHELL_PARAMETERS_PROTOCOL installed:\n");
+		EfiShellParametersProtocol = AllocateZeroPool(sizeof(EFI_SHELL_PARAMETERS_PROTOCOL));
+		EfiShellParametersProtocol->Argc = 2;
+		EfiShellParametersProtocol->Argv[0] = L"test.efi";
+		EfiShellParametersProtocol->Argv[1] = L"-status";
+	}
+
 	status = uefi_call_wrapper(systab->BootServices->HandleProtocol,
 				3,
 				image, 
@@ -65,6 +145,19 @@ efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 	if (EFI_ERROR(status)) {
 		Print(L"Load Image Status = %x\n", status);
 	}
+
+	// pass Argv0, Argv1 with loadOption
+	loaded_image->LoadOptionsSize = 36;
+	loaded_image->LoadOptions = L"test.efi -status";
+
+	// install EFI_SHELL_PARAMETERS_PROTOCOL for the application
+	// which will be loaded
+	status = uefi_call_wrapper(systab->BootServices->InstallProtocolInterface,
+					4,
+					&image_handle,
+					&EfiShellParametersProtocolGuid,
+					EFI_NATIVE_INTERFACE,
+					(void *)EfiShellParametersProtocol);
 
 	status = uefi_call_wrapper(systab->BootServices->StartImage,
 				3,
