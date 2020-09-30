461d949286414722f5774b1b380675a763a7bba0
Ivan Hu <ivan.hu@canonical.com>
2020-09-30 18:25:20 +0800

    tpmevlogdump: add dumping Crypto Agile Log Event
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- tpmevlogdump/tpmevlogdump.c
+++ tpmevlogdump/tpmevlogdump.c
@@ -104,6 +104,24 @@ typedef struct {
 	UINT8	event[];
 } TCG_PCREventStruc;
 
+typedef struct {
+	UINT16 algorithmId;
+	UINT16 digestSize;
+} TCG_EfiSpecIdEventAlgorithmSize;
+
+typedef struct {
+	UINT8 signature[16];
+	UINT32 platformClass;
+	UINT8 specVersionMinor;
+	UINT8 specVersionMajor;
+	UINT8 specErrata;
+	UINT8 uintnSize;
+	UINT32 numberOfAlgorithms;
+	TCG_EfiSpecIdEventAlgorithmSize digestSizes[];
+//	UINT8 vendorInfoSize;
+//	UINT8 vendorInfo;
+} TCG_EfiSpecIDEventStruct;
+
 STATIC UINTN
 GetDigestAlgLength(TPM_ALG_ID AlgId)
 {
@@ -132,6 +150,8 @@ GetDigestAlgLength(TPM_ALG_ID AlgId)
 EFI_STATUS
 efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 {
+
+	int i, j;
 
 	EFI_GUID Guid = EFI_TCG2_PROTOCOL_GUID;
 
@@ -174,33 +194,128 @@ efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 
 		Print (L"first entry event addr 0x%lx\n", StartAddr);
 		Print (L"Last entry event addr 0x%lx\n", LastAddr);
-		/* TBD, Dump event log */
-		Print (L"Dump last entry event\n");
-		UINT64 size = 0;
-		TCG_PCR_EVENT2 *Event2 = (TCG_PCR_EVENT2 *)LastAddr;
 
-		Print(L"  PCR Index: %d\n", Event2->PCRIndex);
-		Print(L"  Event Type: 0x%x\n", Event2->EventType);
-		Print(L"  Digests: %d\n", Event2->Digests.Count);
-		
-		size += sizeof(Event2->PCRIndex) + sizeof(Event2->EventType) + Event2->Digests.Count;
+		/* dump first event (EfiSpecIdEvent) */
+		UINT8 *Pos = (UINT8 *)StartAddr;
+		TCG_PCREventStruc *Event = (TCG_PCREventStruc *)Pos;
+		Print(L"PCR Index: %d\n", Event->pcrIndex);
+		Print(L"Event Type: 0x%x\n", Event->eventType);
+		Print(L"Digest:\n");
+		for (i = 0; i < 20; i++) {				
+			Print(L"%2.2x ", Event->digest[i]);
+		}
+		Print(L"\n");
+		Print(L"Event Size: 0x%x\n", Event->eventDataSize);
+		TCG_EfiSpecIDEventStruct *EventSpecID;
+		EventSpecID = (TCG_EfiSpecIDEventStruct *)Event->event;
+		Print(L"EfiSpecIdEvent:\n");
+		Print(L" Signature: %s\n", (CHAR16 *)EventSpecID->signature);
+		Print(L" platformClass: 0x%x\n", EventSpecID->platformClass);
+		Print(L" specVersionMinor: 0x%x\n", EventSpecID->specVersionMinor);
+		Print(L" specVersionMajor: 0x%x\n", EventSpecID->specVersionMajor);
+		Print(L" specErrata: 0x%x\n", EventSpecID->specErrata);
+		Print(L" uintnSize: 0x%x\n", EventSpecID->uintnSize);
+		Print(L" numberOfAlgorithms: %d\n", EventSpecID->numberOfAlgorithms);
+		Pos += (sizeof(TCG_PCREventStruc) + sizeof(TCG_EfiSpecIDEventStruct));
+		TCG_EfiSpecIdEventAlgorithmSize *SpecIDigestSz = NULL;
+		for (i = 0; i < EventSpecID->numberOfAlgorithms; i++) {
+			SpecIDigestSz = (TCG_EfiSpecIdEventAlgorithmSize *)Pos;
+			Print(L" algorithmId: 0x%x \n", SpecIDigestSz->algorithmId);
+			Print(L" digestSize: 0x%x \n", SpecIDigestSz->digestSize);
+			Pos += sizeof(TCG_EfiSpecIdEventAlgorithmSize);
+		}
+		UINT8 vendorInfoSize = *(UINT8 *)Pos;
+		Print(L" vendorInfoSize: %d\n", vendorInfoSize);
+		Pos += sizeof(UINT8);
+		UINT8 vdata;
+		for (i = 0; i < vendorInfoSize; i++) {
+			if (Pos + vendorInfoSize > (UINT8 *)LastAddr) {
+				Print(L"Event data too large, skip dump\n");
+				return EFI_SUCCESS;
+			}
+			vdata = *(UINT8 *)Pos;
+			Print(L"%2.2x ", vdata);
+			Pos += sizeof(UINT8);
+		}
+		Print(L"\n");
 
-		UINT8 *Pos = (UINT8 *)Event2->Digests.Digests;
+		/* dump others events */
+		TCG_PCR_EVENT2 *Event2 = NULL;
 		UINTN len_digest = 0;
+		UINT8 data;
+		UINT32 EventSize;
+		while (Pos < (UINT8 *)LastAddr) {
+			Event2 = (TCG_PCR_EVENT2 *)Pos;
+			Print(L"PCR Index: %d\n", Event2->PCRIndex);
+			Print(L"Event Type: 0x%x\n", Event2->EventType);
+			Print(L"Digests:\n");
+			Print(L"Digests.Count: %d\n", Event2->Digests.Count);
+			Pos += (sizeof(Event2->PCRIndex) + sizeof(Event2->EventType) + sizeof(Event2->Digests.Count));
+			for (i = 0; i < Event2->Digests.Count; i++) {
+			
+				len_digest = GetDigestAlgLength(((TPMT_HA *)Pos)->Alg_Id);
+				Print(L"  Digests.Alg_ID: 0x%x: \n", ((TPMT_HA *)Pos)->Alg_Id);
+				Print(L"  Digests.Digest:\n");
+				Pos += sizeof(TPMT_HA);
+				for (j = 0; j < len_digest; j++) {
+					if (Pos + len_digest > (UINT8 *)LastAddr) {
+						Print(L"Event data too large, skip dump\n");
+						return EFI_SUCCESS;
+					}
+					data = *(UINT8 *)Pos;
+					Print(L"%02.2x ", data);
+					Pos += sizeof(UINT8);					
+				}
+				Print(L"\n");
+			}
+			EventSize = *(UINT32 *)Pos;
+			Print(L"EventSize: %d\n", EventSize);
+			Pos += sizeof(UINT32);
+			Print(L"Event:\n");
+			if (Pos + EventSize > (UINT8 *)LastAddr) {
+				Print(L"Event data too large, skip dump\n");
+				return EFI_SUCCESS;
+			}
+			for (i = 0; i < EventSize; i++) {
+				data = *(UINT8 *)Pos;
+				Print(L"%02.2x ", data);
+				Pos += sizeof(UINT8);
+			}
+			Print(L"\n");
+		}
+
+		Print (L"Dump last entry event\n");
+		Event2 = (TCG_PCR_EVENT2 *)LastAddr;
+
+		Print(L"PCR Index: %d\n", Event2->PCRIndex);
+		Print(L"Event Type: 0x%x\n", Event2->EventType);
+		Print(L"Digests: %d\n", Event2->Digests.Count);
+
+		Pos = (UINT8 *)Event2->Digests.Digests;
 		for (UINT8 i = 0; i < Event2->Digests.Count; i++) {
 			
 			len_digest = GetDigestAlgLength(((TPMT_HA *)Pos)->Alg_Id);
-			Print(L"  Alg_ID 0x%x: \n", ((TPMT_HA *)Pos)->Alg_Id);
-			/* skip dump digest */
-
-			size += sizeof(TPMT_HA) + len_digest;
-			Pos += sizeof(TPMT_HA) + len_digest;
+			Print(L"  Digests.Alg_ID: 0x%x: \n", ((TPMT_HA *)Pos)->Alg_Id);
+			Print(L"  Digests.Digest:\n");
+			Pos += sizeof(TPMT_HA);
+			for (j = 0; j < len_digest; j++) {
+				data = *(UINT8 *)Pos;
+				Print(L"%02.2x ", data);
+				Pos += sizeof(UINT8);					
+			}
+			Print(L"\n");
 		}
 
-		UINT32 DataSize = *(UINT32 *)Pos;
-		size += sizeof(UINT32);
+		EventSize = *(UINT32 *)Pos;
+		Print(L"EventSize: %d\n", EventSize);
 		Pos += sizeof(UINT32);
-		Print(L"  Event Data Size: %d\n", DataSize);
+		Print(L"Event:\n");
+		for (i = 0; i < EventSize; i++) {
+			data = *(UINT8 *)Pos;
+			Print(L"%02.2x ", data);
+			Pos += sizeof(UINT8);
+		}
+		Print(L"\n");
 
 	} else {
 		Print(L"GetEventLog with format Tcg1.2\n");
@@ -210,7 +325,6 @@ efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 		Print (L"Last entry event addr 0x%lx\n", LastAddr);
 		UINT8 *Pos = (UINT8 *)StartAddr;
 		TCG_PCREventStruc *Event = NULL;
-		int i = 0;
 
 		while (Pos < (UINT8 *)LastAddr) {
 			Event = (TCG_PCREventStruc *)Pos;
@@ -218,17 +332,17 @@ efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 			Print(L"  Event Type: 0x%x\n", Event->eventType);
 			Print(L"  Digest:\n");
 			for (i = 0; i < 20; i++) {				
-				Print(L"%2.2x ", Event->digest[i]);
+				Print(L"%02.2x ", Event->digest[i]);
 			}
 			Print (L"\n");
 			Print(L"  Event Data Size: %d\n", Event->eventDataSize);
 			Print(L"  Event:\n");
-			if (Pos + Event->eventDataSize > LastAddr) {
+			if (Pos + Event->eventDataSize > (UINT8 *)LastAddr) {
 				Print(L"Event data too large, skip dump\n");
 				return EFI_SUCCESS;
 			}
 			for (i = 0; i < Event->eventDataSize; i++) {				
-				Print(L"%2.2x ", Event->event[i]);
+				Print(L"%02.2x ", Event->event[i]);
 			}
 			Pos += (sizeof(TCG_PCREventStruc) + Event->eventDataSize);
 			Print (L"\n");
@@ -245,7 +359,7 @@ efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 		Print(L"  Event Data Size: %d\n", Event->eventDataSize);
 		Print(L"  Event:\n");
 		for (i = 0; i < Event->eventDataSize; i++) {				
-			Print(L"%2.2x ", Event->event[i]);
+			Print(L"%02.2x ", Event->event[i]);
 		}			
 	}
 
